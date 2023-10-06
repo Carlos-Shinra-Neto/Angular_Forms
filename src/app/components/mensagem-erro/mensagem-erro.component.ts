4f427ad2b5b8b862adc8d674b4c181e9767cbe39
Carlos-Shinra-Neto <leosergi09@gmail.com>
2023-10-06 12:34:24 -0300

    Angular Forms

---
+++ src/app/components/mensagem-erro/mensagem-erro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mensagem-erro',
+  templateUrl: './mensagem-erro.component.html',
+  styleUrls: ['./mensagem-erro.component.css']
+})
+export class MensagemErroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
